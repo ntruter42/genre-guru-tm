906c6e948e0ef65a501341fc7150a6ff47c11676
akhona <akhona0304mjikelo@gmail.com>
2023-12-06 08:46:44 +0200

    modified sql file

--- sql/table.sql
+++ sql/table.sql
@@ -11,17 +11,6 @@ CREATE TABLE topic (
     topic_type TEXT NOT NULL
 );
 
-
-CREATE TABLE choice (
-    choice_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id),
-    question_id INT,
-    answer_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (question_id) REFERENCES questions(question_id),
-    FOREIGN KEY (answer_id) REFERENCES answers(answer_id)
-);
-
 CREATE TABLE questions (
     question_id SERIAL PRIMARY KEY,
     level INT NOT NULL,
@@ -32,4 +21,14 @@ CREATE TABLE answers (
     answer_id SERIAL PRIMARY KEY,
     question_id INT REFERENCES questions(question_id),
     correct_choice TEXT NOT NULL
+);
+
+CREATE TABLE choice (
+    choice_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    question_id INT,
+    answer_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (question_id) REFERENCES questions(question_id),
+    FOREIGN KEY (answer_id) REFERENCES answers(answer_id)
 );
