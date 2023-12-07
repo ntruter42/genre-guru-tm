936760c21ada1c86db6d508732261497e6de7314
ntruter42 <ntruter42@gmail.com>
2023-12-07 10:32:06 +0200

    Removed unnecessary tables

---
+++ sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO topic (topic_name) VALUES ('Jazz');
+INSERT INTO topic (topic_name) VALUES ('Hip-Hop');
+INSERT INTO topic (topic_name) VALUES ('Amapiano');
+INSERT INTO topic (topic_name) VALUES ('Classical');
+INSERT INTO topic (topic_name) VALUES ('Reggae');
--- sql/topic.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO topic (topic_name) VALUES ('Jazz');
-INSERT INTO topic (topic_name) VALUES ('Hip-hop');
-INSERT INTO topic (topic_name) VALUES ('Amapiano');
-INSERT INTO topic (topic_name) VALUES ('RnB');
