1c7398d1d2834ccebe911efa8c58162c687f25f1
ntruter42 <ntruter42@gmail.com>
2023-12-07 10:34:18 +0200

    Fixed table names

--- sql/data.sql
+++ sql/data.sql
@@ -1,5 +1,29 @@
-INSERT INTO topic (topic_name) VALUES ('Jazz');
-INSERT INTO topic (topic_name) VALUES ('Hip-Hop');
-INSERT INTO topic (topic_name) VALUES ('Amapiano');
-INSERT INTO topic (topic_name) VALUES ('Classical');
-INSERT INTO topic (topic_name) VALUES ('Reggae');
+INSERT INTO guru.topics (topic_name) VALUES ('Jazz');
+INSERT INTO guru.topics (topic_name) VALUES ('Hip-Hop');
+INSERT INTO guru.topics (topic_name) VALUES ('Amapiano');
+INSERT INTO guru.topics (topic_name) VALUES ('Classical');
+INSERT INTO guru.topics (topic_name) VALUES ('Reggae');
+
+INSERT INTO guru.questions (question, level, topic_id) VALUES ('Who is considered the "King of Jazz" and was known for his trumpet playing?', 2 ,1 );
+INSERT INTO guru.questions (question, level, topic_id) VALUES('What is the name of the famous annual jazz festival held in New Orleans?',1,1);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which influential jazz pianist is known for his improvisational style and compositions such as Take Five?',2,1);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('What is scat singing, and which jazz artist popularized this vocal technique?',3,1);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which city is often referred to as the birthplace of jazz?',1,1 );
+
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Who is often credited with being the "Godfather of Hip-Hop"?',1,2)
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which borough of New York City is considered the birthplace of hip-hop?',1,2);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('What is the name of the first commercially successful rap song?',2,2);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which hip-hop artist founded the record label "Death Row Records"?',2,2)
+INSERT INTO guru.questions (question, level, topic_id) VALUES('What are the four elements that form the foundation of hip-hop culture?',3,2)
+
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Who is often referred to as the "King of R&B"?',1,4)
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which city is known as the birthplace of Motown Records, a significant R&B label?',1,4);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which female R&B artist released the album "The Miseducation of Lauryn Hill"?',2,4);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('What does R&B stand for in the context of music?',1,4)
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which male R&B artist is known for hits like "Ignition (Remix)',1,4)
+
+INSERT INTO guru.questions (question, level, topic_id) VALUES('What country is amapiano music from?',1,3)
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Who is considered one of the pioneers of amapiano music?',2,3);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which South African DJ and producer is known for popularizing amapiano',2,3);
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Which musical elements characterize amapiano music?',3,3)
+INSERT INTO guru.questions (question, level, topic_id) VALUES('Name a popular amapiano hit that gained international recognition',2,3)
