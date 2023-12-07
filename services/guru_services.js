9082e00125d31e51efe50ce51873bbbb75df925f
akhona <akhona0304mjikelo@gmail.com>
2023-12-07 10:42:00 +0200

    Reset services files

--- services/guru_services.js
+++ services/guru_services.js
@@ -6,33 +6,7 @@ export default function (db) {
 		return result;
 	}
 
-	async function getAnswers() {
-		let query = ``;
-
-		return true;
-	}
-
 	return {
-		returnTrue,
-		getAnswers
+		returnTrue
 	};
 }
-function storeSongData(title, artist, genre) {
-	let songDatabase;
-	const songData = {
-	  title: title,
-	  artist: artist,
-	  genre: genre
-	};
-  
-	// Assuming songDatabase is an array where you store each song's data
-	songDatabase.push(songData);
-  
-	// You can also store the data in a database using a database query here
-	
-	// For example, if using MongoDB:
-	// db.collection('songs').insertOne(songData);
-  
-	// You may want to perform additional operations or error handling as needed
-  }
-  
